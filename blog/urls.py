6c88b86659f645abf37e31b89f7481a3c94e6537
Matthias Kestenholz <mk@spinlock.ch>
2012-12-13 10:13:51 +0100

    Actually reevaluate Post.objects.published() when accessing views

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.views import generic
 
-from .models import Category, Post
+from . import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', generic.ArchiveIndexView.as_view(
-        date_field='published_on',
-        queryset=Post.objects.published(),
-        ), name='blog_post_archive'),
+    url(r'^$', views.ArchiveIndexView.as_view(), name='blog_post_archive'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[^/]+)/$',
-        generic.DateDetailView.as_view(
-            date_field='published_on',
-            month_format='%m',
-            queryset=Post.objects.published(),
-            ), name='blog_post_detail'),
+        views.DateDetailView.as_view(), name='blog_post_detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.views import generic
+
+from .models import Category, Post
+
+
+class PostMixin(object):
+    date_field = 'published_on'
+    month_format = '%m'
+
+    def get_queryset(self):
+        return Post.objects.published()
+
+
+class ArchiveIndexView(PostMixin, generic.ArchiveIndexView):
+    pass
+
+
+class DateDetailView(PostMixin, generic.DateDetailView):
+    pass
