515e74595fc95c69be271ff21ed2409cc7aab82a
Matthias Kestenholz <mk@spinlock.ch>
2012-12-13 14:30:32 +0100

    Add a simple fabfile

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+import os
+from fabric.api import *
+
+env.forward_agent = True
+env.hosts = ['406.ch']
+
+@task
+def deploy():
+    local('git push origin master')
+    with cd('/var/www/mk/406'):
+        run('git checkout master') # yes.
+        run('git pull')
+        run('find . -name "*.pyc" -delete')
+        run('venv/bin/pip install -r requirements/live.txt')
+        run('venv/bin/python manage.py migrate')
+        run('venv/bin/python manage.py syncdb')
+        with cd('mkweb/static/406'):
+            run('compass compile')
+        run('venv/bin/python manage.py collectstatic --noinput')
+        run('sudo supervisorctl restart mk_406')
