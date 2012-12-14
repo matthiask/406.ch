c107655783fd8beb81a6c1875b8b71e727ee1a96
Matthias Kestenholz <mk@spinlock.ch>
2012-12-14 09:25:27 +0100

    Redirect home page to blog

--- mkweb/urls.py
+++ mkweb/urls.py
@@ -7,11 +7,10 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mkweb.views.home', name='home'),
-    # url(r'^mkweb/', include('mkweb.foo.urls')),
-
-    url(r'^$', generic.TemplateView.as_view(
+    url(r'^$', generic.RedirectView.as_view(
+        url='/blog/',
+        )),
+    url(r'^home/$', generic.TemplateView.as_view(
         template_name='home.html',
         )),
     url(r'^404/$', generic.TemplateView.as_view(
