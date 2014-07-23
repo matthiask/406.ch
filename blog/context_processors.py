e646886a0a3bb9cbcab1960b6744e9d1a036ba1a
Matthias Kestenholz <mk@spinlock.ch>
2014-07-23 14:54:18 +0200

    Simplify the archive and fix some spacing

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,7 +1,4 @@
-from collections import defaultdict
-
 from django.core.urlresolvers import reverse
-from django.db.models import Count
 
 from .models import Category, Post
 
@@ -13,38 +10,11 @@ class ContextObject(object):
     def categories(self):
         return Category.objects.all()
 
-    def posts_by_category(self):
-        items = Post.categories.through.objects.filter(
-            post__in=Post.objects.published(),
-        ).values(
-            'category__title',
-            'category__slug',
-        ).annotate(Count('post')).order_by('category__title')
-
-        for row in items:
-            yield (
-                row['category__title'],
-                row['post__count'],
-                reverse('blog_category_detail', kwargs={
-                    'slug': row['category__slug'],
-                }),
-            )
-
-    def posts_by_month(self):
-        months = defaultdict(int)
-        for date in Post.objects.published().values_list(
-                'published_on', flat=True):
-            months[date.date().replace(day=1)] += 1
-
-        for date, count in sorted(months.items(), reverse=True):
-            yield (
-                date,
-                count,
-                reverse('blog_post_archive_month', kwargs={
-                    'year': date.strftime('%Y'),
-                    'month': date.strftime('%m'),
-                }),
-            )
+    def years_with_posts(self):
+        return [
+            date.year
+            for date in Post.objects.published().datetimes(
+                'published_on', 'year', 'DESC')]
 
     def feed_url(self):
         return self.request.build_absolute_uri(reverse('blog_post_feed'))
