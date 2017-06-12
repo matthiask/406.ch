63a8baea354887584bdaed08e96b64f4cb755442
Matthias Kestenholz <mk@feinheit.ch>
2017-06-12 15:55:41 +0200

    Import the gfm mode

--- mkweb/static/mkweb/admin.js
+++ mkweb/static/mkweb/admin.js
@@ -1,5 +1,6 @@
 import CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/gfm/gfm.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   let areas = Array.from(document.querySelectorAll('textarea'));
