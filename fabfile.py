71b4c55d9e79e3eb830427151da7401ab9d12006
Matthias Kestenholz <mk@feinheit.ch>
2021-11-14 17:07:57 +0100

    Backend update

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import fh_fablib as fl
 
 
-fl.require("1.0.20210506")
+fl.require("1.0.20211029")
 fl.config.update(base=fl.Path(__file__).parent, host="www-data@feinheit06.nine.ch")
 fl.config.update(app="app", domain="406.ch", branch="master", remote="production")
 
