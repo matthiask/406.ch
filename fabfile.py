f7e2679e65b49d84d7f9dd6bf2dfc44b86c764ea
Matthias Kestenholz <mk@feinheit.ch>
2022-11-11 21:39:02 +0100

    Rename

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,6 @@ import fh_fablib as fl
 
 fl.require("1.0.20221102")
 fl.config.update(host="www-data@feinheit06.nine.ch")
-fl.config.update(domain="406.ch", branch="master", remote="production")
+fl.config.update(domain="406.ch", branch="main", remote="production")
 
 ns = fl.Collection(*fl.GENERAL, *fl.NINE)
