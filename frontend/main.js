3ed11922d6c2b1d9ea352122853e7fa23cef9f40
Matthias Kestenholz <mk@feinheit.ch>
2023-04-16 14:17:13 +0200

    Drop the old code

--- frontend/main.js
+++
@@ -1,7 +0,0 @@
-import "./reset.css"
-import "./main.css"
-
-// import "blissfuljs"
-// const { $, $$ } = window // eslint-disable-line
-
-// document.addEventListener("DOMContentLoaded", function () {})
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import speckenv
-from django.core.wsgi import get_wsgi_application
-
-
-speckenv.read_speckenv()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
-application = get_wsgi_application()
