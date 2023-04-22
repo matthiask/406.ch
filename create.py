fa4098c7ea2030227ff611ca3e95c0a246356ab4
Matthias Kestenholz <mk@feinheit.ch>
2023-04-22 10:39:31 +0200

    Add a minimally more interactive and understandable post creation script

--- create.py
+++ create.py
@@ -6,26 +6,33 @@ import sys
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent
-
-
 def slugify(value):
     return re.sub(r"[^-a-z0-9]+", "-", value.lower()).strip("-")
 
 
-type = "published" if len(sys.argv) < 2 else sys.argv[1]
-assert type in {"draft", "published"}, "First argument must be 'draft' or 'published'"
+while True:
+    type = input("Create a draft? [Y/n] ").lower()
+    if type in {"", "y", "n"}:
+        type = "published" if type == "n" else "draft"
+        break
+while True:
+    title = input("Title: ")
+    if title:
+        break
 
-if title := input("Title: "):
-    pubdate = dt.date.today().isoformat()
-    filename = f"{pubdate}-{slugify(title)}.md"
-    (BASE_DIR / type / filename).write_text(
-        f"""\
+pubdate = dt.date.today().isoformat()
+filename = f"{pubdate}-{slugify(title)}.md"
+text = f"""\
 Title: {title}
 Date: {pubdate}
 Categories:
 
 # {title}
 """
-    )
+
+path = Path.cwd() / type / filename
+if path.exists():
+    print(f"Not overwriting {type}/{filename}", file=sys.stderr)
+else:
+    path.write_text(text)
     print(f"Created {type}/{filename}")
