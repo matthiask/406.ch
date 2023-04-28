19b4f461f5df2c6d952a24b93a84b7b419c907c8
Matthias Kestenholz <mk@feinheit.ch>
2023-04-28 19:52:24 +0200

    Nice

--- generate.py
+++ generate.py
@@ -17,8 +17,8 @@ from minify_html import minify
 from rcssmin import cssmin
 
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent
-BASE = "https://406.ch"
+DIR = Path(__file__).resolve(strict=True).parent
+URL = "https://406.ch"
 TITLE = "Matthias Kestenholz"
 
 
@@ -57,7 +57,7 @@ def parse_categories(value):
 
 
 def load_posts(dirs):
-    for md in chain.from_iterable(BASE_DIR.glob(f"{dir}/*.md") for dir in dirs):
+    for md in chain.from_iterable(DIR.glob(f"{dir}/*.md") for dir in dirs):
         try:
             props, content = md.read_text().replace("\r", "").split("\n\n", 1)
             props = [re.split(r":\s*", prop, 1) for prop in props.split("\n")]
@@ -75,26 +75,26 @@ def load_posts(dirs):
                 body=body,
             )
         except Exception as exc:
-            md = md.relative_to(BASE_DIR)
+            md = md.relative_to(DIR)
             print(f"Skipping the invalid '{md}' file: {exc!r}", file=sys.stderr)
 
 
 def write_file(path, content):
-    file = BASE_DIR / "htdocs" / path.lstrip("/")
+    file = DIR / "htdocs" / path.lstrip("/")
     file.parent.mkdir(parents=True, exist_ok=True)
     file.write_text(content)
     write_file.count = getattr(write_file, "count", 0) + 1
 
 
 def styles():
-    styles = cssmin("".join(file.read_text() for file in BASE_DIR.glob("styles/*.css")))
+    styles = cssmin("".join(file.read_text() for file in DIR.glob("styles/*.css")))
     style_file = f"/styles.{md5(styles.encode('utf-8')).hexdigest()[:12]}.css"
     write_file(style_file, styles)
     return style_file
 
 
 def jinja_templates(**kwargs):
-    loader = FileSystemLoader([BASE_DIR / "templates"])
+    loader = FileSystemLoader([DIR / "templates"])
     env = Environment(loader=loader, autoescape=True)
     env.globals.update({"year": date.today().year, "styles": styles()} | kwargs)
     r = lambda template: lambda **ctx: minify(template.render(**ctx))  # noqa: E731
@@ -104,7 +104,7 @@ def jinja_templates(**kwargs):
 def write_feed_with_posts(path, posts, title, link):
     root = Element("feed", {"xml:lang": "en", "xmlns": "http://www.w3.org/2005/Atom"})
     SubElement(root, "title").text = title
-    SubElement(root, "link", {"href": f"{BASE}/{path}atom.xml", "rel": "self"})
+    SubElement(root, "link", {"href": f"{URL}/{path}atom.xml", "rel": "self"})
     SubElement(root, "link", {"href": link, "rel": "alternate"})
     SubElement(root, "id").text = link
     SubElement(root, "updated").text = posts[0].updated
@@ -112,7 +112,7 @@ def write_feed_with_posts(path, posts, title, link):
     for post in posts:
         entry = SubElement(root, "entry")
         SubElement(entry, "title").text = post.title
-        link = f"{BASE}{post.url()}"
+        link = f"{URL}{post.url()}"
         SubElement(entry, "link", {"href": link, "rel": "alternate"})
         SubElement(entry, "id").text = link
         SubElement(entry, "published").text = post.updated
@@ -127,7 +127,7 @@ def write_feed_with_posts(path, posts, title, link):
 def write_sitemap(posts):
     root = Element("urlset", {"xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9"})
     for post in posts:
-        SubElement(SubElement(root, "url"), "loc").text = f"{BASE}{post.url()}"
+        SubElement(SubElement(root, "url"), "loc").text = f"{URL}{post.url()}"
     xml = tostring(root, encoding="utf-8", xml_declaration=True).decode("utf-8")
     write_file("sitemap.xml", xml)
 
@@ -137,15 +137,15 @@ if __name__ == "__main__":
     categories = sorted(set(chain.from_iterable(post.categories for post in posts)))
     print(f"{len(posts)} posts in {', '.join(c.title for c in categories)}.")
 
-    shutil.rmtree(BASE_DIR / "htdocs", ignore_errors=True)
-    write_file("writing/index.html", f'<meta content="0;url={BASE}"http-equiv=refresh>')
-    write_file("robots.txt", f"User-agent: *\nSitemap: {BASE}/sitemap.xml\n")
+    shutil.rmtree(DIR / "htdocs", ignore_errors=True)
+    write_file("writing/index.html", f'<meta content="0;url={URL}"http-equiv=refresh>')
+    write_file("robots.txt", f"User-agent: *\nSitemap: {URL}/sitemap.xml\n")
     write_sitemap(posts)
 
     archive, detail, not_found = jinja_templates(categories=categories)
     write_file("404.html", not_found())
     write_file("index.html", archive(posts=posts))
-    write_feed_with_posts("writing/", posts[:20], title=TITLE, link=f"{BASE}/")
+    write_feed_with_posts("writing/", posts[:20], title=TITLE, link=f"{URL}/")
     for post in posts:
         write_file(f"{post.url()}index.html", detail(post=post))
 
@@ -159,6 +159,6 @@ if __name__ == "__main__":
             category.url().lstrip("/"),
             category_posts[:20],
             title=f"{TITLE}: Posts about {category.title}",
-            link=f"{BASE}{category.url()}",
+            link=f"{URL}{category.url()}",
         )
     print(f"Wrote {write_file.count} files.")
