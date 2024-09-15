692b3be7438c2f3af4d7b671cf74784577960096
Matthias Kestenholz <mk@feinheit.ch>
2024-09-15 18:09:19 +0200

    Bye tox, hello uv

--- watch.sh
+++ watch.sh
@@ -6,7 +6,7 @@ export PYTHONWARNINGS=always
 export PYTHONUNBUFFERED=yes
 
 set -ex
-(cd htdocs; python3 -m http.server 8001)&
-(git ls-files | entr tox)&
+(cd htdocs; python3 -m http.server 8001) &
+(git ls-files | entr uv run python -Wd -c 'import generate as g; g.main(only_published=False)') &
 
 for job in $(jobs -p); do wait $job; done
