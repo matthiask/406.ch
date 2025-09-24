44629220cae1659d84ec0ef1292f505af0089927
Matthias Kestenholz <mk@feinheit.ch>
2025-09-24 15:24:28 +0200

    Write

--- watch.sh
+++ watch.sh
@@ -6,7 +6,7 @@ export PYTHONWARNINGS=always
 export PYTHONUNBUFFERED=yes
 
 set -ex
-(cd htdocs; npx serve) &
+(mkdir -p htdocs && cd htdocs && npx serve) &
 (git ls-files | entr uv run python -Wd -c 'import generate as g; g.main(only_published=False, base_url="http://localhost:3000")') &
 
 for job in $(jobs -p); do wait $job; done
