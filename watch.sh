37cb9d1005d88c818fe3e04fbf27c385b88f5f8d
Matthias Kestenholz <mk@feinheit.ch>
2026-03-25 13:17:22 +0100

    Oops

--- watch.sh
+++ watch.sh
@@ -7,6 +7,6 @@ export PYTHONUNBUFFERED=yes
 
 set -ex
 (mkdir -p htdocs && cd htdocs && uv run python -m http.server 8001) &
-(git ls-files | entr uv run python -Wd -c 'import generate as g; g.main(only_published=False, base_url="http://localhost:3000")') &
+(git ls-files | entr uv run python -Wd -c 'import generate as g; g.main(only_published=False, base_url="http://localhost:8001")') &
 
 for job in $(jobs -p); do wait $job; done
