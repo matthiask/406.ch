c653a612c12377db493ae61b2e7c1ba0c7e7664e
Matthias Kestenholz <mk@feinheit.ch>
2026-03-25 13:16:44 +0100

    Stop depending on npx

--- watch.sh
+++ watch.sh
@@ -6,7 +6,7 @@ export PYTHONWARNINGS=always
 export PYTHONUNBUFFERED=yes
 
 set -ex
-(mkdir -p htdocs && cd htdocs && npx serve) &
+(mkdir -p htdocs && cd htdocs && uv run python -m http.server 8001) &
 (git ls-files | entr uv run python -Wd -c 'import generate as g; g.main(only_published=False, base_url="http://localhost:3000")') &
 
 for job in $(jobs -p); do wait $job; done
